ad6ace26fea20db43202c2885e5ab6fdc2d28590
Anguie02 <1487186@senati.pe>
2023-02-23 10:56:00 -0500

    Pedimos el ingreso de números

--- note.py
+++ note.py
@@ -1,2 +1,6 @@
 #mensaje de bienvenida
 print("Welcome back")
+#suma  y multiplicación
+#Pedimos el igreso de numeros
+a = int(input("Ingese el primer número : "))
+b = int(input("Ingrese el segundo número"))
