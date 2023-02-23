c1ad4d8163c02e9fba62f17912f2d0075ebf4b2f
Anguie02 <1487186@senati.pe>
2023-02-23 11:04:19 -0500

    Mostramos la multiplicación

--- note.py
+++ note.py
@@ -3,7 +3,7 @@ print("Welcome back")
 #suma  y multiplicación
 #Pedimos el igreso de numeros
 a = int(input("Ingese el primer número : "))
-b = int(input("Ingrese el segundo número"))
+b = int(input("Ingrese el segundo número: "))
 #proceso de la suma 
 suma = a + b
 #proceso de la multiplicación
@@ -11,3 +11,4 @@ producto = a * b
 
 #mostramos la suma y multiplicación de los números
 print("La suma es: ", suma)
+print("La multiplicación es: ", producto)
