2dc511cef1799ae86692444dd43dac866a719b9c
Anguie02 <1487186@senati.pe>
2023-02-24 09:55:08 -0500

    Se realizo la suma del numero actual y el anterior

--- ejercicio1_python/ejercico _suma.py
+++ ejercicio1_python/ejercico _suma.py
@@ -2,3 +2,9 @@
 
 #Pedimos el ingreso del numero
 numero = int(input("Ingrese un número :"))
+#creamos un bucle 
+for a in range(1, numero):
+    numero = a
+    #Realizamos la suma del numero actual y el anterior
+    suma = numero + (numero -1)
+
