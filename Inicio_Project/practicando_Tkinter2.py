c0750cb7794b806d6cfc71bf526d1c4b420eb6d0
Anguie02 <1487186@senati.pe>
2023-02-28 00:14:19 -0500

    Añadimos un caja para presionar

--- Inicio_Project/practicando_Tkinter2.py
+++ Inicio_Project/practicando_Tkinter2.py
@@ -16,6 +16,9 @@ caja2.place(x=10,y=50,width=100,height=30)
 #Segunda caja de texto
 caja_text2= Entry(vent_principal,bg="pink")
 caja_text2.place(x=120,y=50,width=100,height=30)
+#Añadimos un boton
+caja_suma = Button(vent_principal,text="Sumar")
+caja_suma.place(x=230,y=50,width=80,height=30)
 
 caja3 = Label(vent_principal, text="Resultado",bg="sky blue")
 caja3.place(x=10,y=120,width=100,height=30)
