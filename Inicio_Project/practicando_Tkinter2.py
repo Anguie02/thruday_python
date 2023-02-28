c10920cb121d692b332b64e444e0b019ce34b03f
Anguie02 <1487186@senati.pe>
2023-02-27 23:46:25 -0500

    Añadir Label y su ubicación

--- Inicio_Project/practicando_Tkinter2.py
+++ Inicio_Project/practicando_Tkinter2.py
@@ -4,8 +4,9 @@ from tkinter import Tk, Label, Button, Entry
 vent_principal = Tk()
 vent_principal.title("Hello back")
 vent_principal.geometry("600x400")
-
-
+#Añadimos Label  y lo ubicamos
+caja = Label(vent_principal,text="Primer numero",bg="sky blue")
+caja.place(x=10,y=10,width=100,height=30)
 
 
 vent_principal.mainloop()
