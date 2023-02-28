2b1095db7c771e3fb40f89dafc07206e0297ff39
Anguie02 <1487186@senati.pe>
2023-02-27 23:38:21 -0500

    Importar clases especificas

---
+++ Inicio_Project/practicando_Tkinter2.py
@@ -0,0 +1,11 @@
+#importamos las clases que queremos usar
+from tkinter import Tk, Label, Button, Entry
+#creamos la ventana principal
+vent_principal = Tk()
+vent_principal.title("Hello back")
+vent_principal.geometry("600x400")
+
+
+
+
+vent_principal.mainloop()
