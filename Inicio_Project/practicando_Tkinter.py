53fcf6f2968cb35bd5fdc6e6b381cef10e8f8f40
Anguie02 <1487186@senati.pe>
2023-02-27 22:57:28 -0500

    Añadir controles(Label)

--- Inicio_Project/practicando_Tkinter.py
+++ Inicio_Project/practicando_Tkinter.py
@@ -6,5 +6,8 @@ ventana = Tk()
 ventana.title("Hello !")
 #configuramos las dimenciones de la ventana
 ventana.geometry("600x400")
+#añadimos widgest o controles que van dentro de la ventana
+etiqueta = Label(ventana, text="Esto es un label")
+etiqueta.pack()
 #añadimos el bucle
 ventana.mainloop()
