156a85f7e995ced7e8b799e80d408cfd008e1277
Anguie02 <1487186@senati.pe>
2023-02-27 23:16:06 -0500

    Agregar una función como parametro

--- Inicio_Project/practicando_Tkinter.py
+++ Inicio_Project/practicando_Tkinter.py
@@ -1,5 +1,8 @@
 #importamos Tkinter
 from tkinter import *
+#creando función
+def mifuncion():
+    print("Presionaste click")
 #creamos la raiz o ventana a partir de la clase Tk
 ventana = Tk()
 #añadimos un titulo
@@ -10,7 +13,7 @@ ventana.geometry("600x400")
 etiqueta = Label(ventana, text="Esto es un label")
 etiqueta.pack()
 #añadimos  otro widget que será el botón
-botón = Button(ventana, text="CLICK")
+botón = Button(ventana, text="CLICK", command=mifuncion)#creamos una función para que sea ejecutada
 botón.pack()#lo empaquetamos para que funcione
-#añadimos el bucle
+#añadimos el bucles
 ventana.mainloop()
