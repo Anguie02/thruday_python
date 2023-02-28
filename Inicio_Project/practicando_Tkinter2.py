dedf466ab83e8e22cba9f7ae9275ca5fabdd134b
Anguie02 <1487186@senati.pe>
2023-02-27 23:54:53 -0500

    Se añadió dos Label

--- Inicio_Project/practicando_Tkinter2.py
+++ Inicio_Project/practicando_Tkinter2.py
@@ -7,6 +7,11 @@ vent_principal.geometry("600x400")
 #Añadimos Label  y lo ubicamos
 caja = Label(vent_principal,text="Primer numero",bg="sky blue")
 caja.place(x=10,y=10,width=100,height=30)
+#Añadimos otros Label
+caja2 = Label(vent_principal,text="Segundo número", bg="sky blue")
+caja2.place(x=10,y=50,width=100,height=30)
 
+caja3 = Label(vent_principal, text="Resultado",bg="sky blue")
+caja3.place(x=10,y=120,width=100,height=30)
 
 vent_principal.mainloop()
